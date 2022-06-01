4f0cbe9e67ecd086c28840a015be723c6d79fee7
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-06-01 01:42:01 -0700

    Add files via upload

---
+++ Mortgage App/MortgageV0/main/java/com/example/mortgagev0/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mortgagev0;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
