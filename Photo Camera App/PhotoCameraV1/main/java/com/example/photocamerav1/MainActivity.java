c010984fdd847419b601a202e32a3a783b66f8b6
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-06-19 11:16:07 -0700

    Add files via upload

---
+++ Photo Camera App/PhotoCameraV1/main/java/com/example/photocamerav1/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.photocamerav1;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button takePhoto;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    private ImageView imageView;
+    private BitmapGrayer grayer;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get reference to imageView
+        imageView = (ImageView) findViewById(R.id.picture);
+
+        activityResultLauncher = registerForActivityResult(new
+                ActivityResultContracts.StartActivityForResult(), new
+                ActivityResultCallback<ActivityResult>() {
+                    // Called when a user takes a picture
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                            Bundle bundle = result.getData().getExtras();
+                            Bitmap bitmap = (Bitmap) bundle.get("data");
+
+                            // "equal" coefficients for red, green, and blue
+                            grayer = new BitmapGrayer(bitmap, 0.34f, 0.33f, 0.33f);
+
+                            // grayscale bitmap
+                            bitmap = grayer.grayScale();
+
+                            // set ImageView as the grayscaled image
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    }
+                });
+
+        // Check whether a device has a given feature (camera, bluetooth, gps, etc)
+        PackageManager manager = this.getPackageManager();
+
+        if (manager.hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            // Toast to show that a camera exists and that the if statement executed
+            Toast.makeText(MainActivity.this, "There is A CAMERA", Toast.LENGTH_SHORT).show();
+
+            // Reference to a button of the photo taken
+            takePhoto = findViewById(R.id.take_Photo);
+
+            // Set up event handling
+            takePhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Launch camera activity
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (intent.resolveActivity(getPackageManager()) != null)
+                    {
+                        activityResultLauncher.launch(intent);
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this,
+                            "There is no app that support this action",
+                            Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+    }
+}
